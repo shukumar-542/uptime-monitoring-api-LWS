cea08a87dbb9630800b8f69e311f81cab514fffe
unknown <shukumar542@gmail.com>
2022-11-22 20:28:54 +0600

    check user crud operation

--- routes.js
+++ routes.js
@@ -3,12 +3,14 @@
 // dependencies
 const {samplaHandlers} = require('./handlers/routeHandlers/samplaHandlers');
 const {userHandlers} = require('./handlers/routeHandlers/userHandler');
-const {tokenHandlers} = require('./handlers/routeHandlers/tokenHandlers')
+const {tokenHandlers} = require('./handlers/routeHandlers/tokenHandlers');
+const {checkHandlers} = require('./handlers/routeHandlers/checkHandler')
 
 const routes ={
       sample:samplaHandlers,
       user :userHandlers,
       token : tokenHandlers,
+      check : checkHandlers,
       
 }
 
